f537711b7280f210377938825b24413dba80e346
Rob Norris <rob_norris@mac.com>
2015-01-08 22:00:23 -0800

    initial commit

---
+++ build.sbt
@@ -0,0 +1,31 @@
+
+organization := "org.tpolecat"
+
+name := "doobie-remotely"
+
+version := "0.1.0-SNAPSHOT"
+
+licenses ++= Seq(("MIT", url("http://opensource.org/licenses/MIT")))
+
+scalaVersion := "2.10.4"
+
+scalacOptions ++= Seq(
+  "-encoding", "UTF-8", // 2 args
+  "-feature",                
+  "-language:existentials",
+  "-language:higherKinds",
+  "-language:implicitConversions",
+  "-language:experimental.macros",
+  "-unchecked",
+  "-Xlint",
+  "-Yno-adapted-args",       
+  "-Ywarn-dead-code",       
+  "-Ywarn-value-discard"     
+)
+
+libraryDependencies ++= Seq(     
+  "oncue.svc.remotely" %% "core"        % "1.1-SNAPSHOT",
+  "org.tpolecat"       %% "doobie-core" % "0.2.0-SNAPSHOT",
+  "org.postgresql"     %  "postgresql"  % "9.3-1102-jdbc41"
+)
+
---
+++ src/main/scala/demo/World.scala
@@ -0,0 +1,65 @@
+package demo
+
+import doobie.imports._
+import doobie.util.invariant.UnexpectedEnd
+import java.net.InetSocketAddress
+import java.util.concurrent.Executors
+import remotely._
+import remotely.codecs._
+import remotely.Remote.implicits._
+import remotely.transport.netty.NettyTransport
+import scala.reflect.runtime.universe.TypeTag
+import scalaz.\/
+import scalaz.concurrent.Task
+import scodec.Codec
+
+object World {
+
+  implicit val ResponseCapture =
+    new Capture[Response] {
+      def apply[A](a: => A) = Response.delay(a)
+    }
+
+  val xa = DriverManagerTransactor[Response](
+    "org.postgresql.Driver", "jdbc:postgresql:world", "rnorris", ""
+  )
+
+  def pop(name: String): Response[String \/ Int] = 
+    sql"select population from country where name = $name"
+      .query[Int]
+      .unique
+      .attemptSome { case UnexpectedEnd => s"No such country: $name" }
+      .transact(xa)
+
+  val env = Environment.empty
+    .codec[String]
+    .codec[String \/ Int]
+    .populate { _
+      .declare("pop", pop _)
+    }
+
+}
+
+object WorldMain extends App {
+  import World.env
+  
+  val addr = new InetSocketAddress("localhost", 8083)
+  val server = env.serveNetty(addr, Executors.newCachedThreadPool, Monitoring.consoleLogger("[server]"))
+  val transport = NettyTransport.single(addr)
+  val loc: Endpoint = Endpoint.single(transport)
+  
+  def result[A: TypeTag: Codec](r: Remote[A]): Task[A] =
+    r.runWithContext(loc, Response.Context.empty, Monitoring.consoleLogger("[client]"))
+
+  val pop = Remote.ref[String => String \/ Int]("pop")
+
+  try { 
+    result(pop("France")).run
+    result(pop("Chickenbutt")).run
+    result(pop("Canada")).run 
+  } finally {
+    transport.shutdown()
+    server()
+  }
+
+}
